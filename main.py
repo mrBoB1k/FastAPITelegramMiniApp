758d5904f06fa95e398d78d69c9618d8227f70c0
Artyom Tretyavko <tr_artem@vk.com>
2025-05-04 21:13:16 +0500

    test ws

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from pydantic import BaseModel
-
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 
 class User(BaseModel):
@@ -10,7 +10,46 @@ class User(BaseModel):
     last_name: str = None
     phone_number: str  = None
 
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
 @app.get("/")
+async def get():
+    return HTMLResponse(html)
+@app.get("/hello")
 async def root():
     return {"message": "Hello World"}
 
@@ -23,9 +62,13 @@ async def say_hello(name: str):
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await websocket.send_text(f"Echo: {data}")
-    except WebSocketDisconnect:
-        print("Client disconnected")
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
+
+@app.websocket("/ws/{id_interactive}")
+async def websocket_endpoint(websocket: WebSocket, id_interactive: int, telegram_id: int):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
