21bb2d50f081a045c1da9f65a8a01f07f9a7a57a
Artyom Tretyavko <tr_artem@vk.com>
2025-05-04 21:40:56 +0500

    test ws

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ html = """
         <script>
             var client_id = Date.now()
             document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://carclicker.ru:8000/ws/${client_id}`);
+            var ws = new WebSocket(`wss://${location.host}/ws/${client_id}`);
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
