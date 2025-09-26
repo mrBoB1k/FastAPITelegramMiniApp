2dcd7f921db9cb68775cf571a73d0685b7e4b943
Artyom Tretyavko <tr_artem@vk.com>
2025-09-26 22:01:46 +0500

    Migrating from SQLite to PostgreSQL

--- src/database.py
+++ src/database.py
@@ -3,14 +3,28 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from models import Base
 
-DATABASE_FILE = "database.db"
-DATABASE_URL = f"sqlite+aiosqlite:///{DATABASE_FILE}"
+# Определяем среду выполнения
+ENVIRONMENT = os.getenv("ENVIRONMENT")
+
+if ENVIRONMENT == "production":
+    # PostgreSQL для продакшена
+    DB_HOST = os.getenv("DB_HOST")  # имя сервиса в docker-compose
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+    DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+else:
+    # SQLite для разработки
+    DATABASE_FILE = "database.db"
+    DATABASE_URL = f"sqlite+aiosqlite:///{DATABASE_FILE}"
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 new_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
 async def init_db():
-    if not os.path.exists("database.db"):
-        async with engine.begin() as conn:
-            await conn.run_sync(Base.metadata.create_all)
+    """Инициализация базы данных"""
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
